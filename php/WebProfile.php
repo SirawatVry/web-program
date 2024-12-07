d5c0e55f16997b1f4d9c14dfeaa7c1769354d454
SirawatVry <s6606021630061@email.kmutnb.ac.th>
2024-12-07 14:36:02 +0700

    web

---
+++ php/WebProfile.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+    <?php
+        $namenn = "ชื่อ นาย ศิรวัฒน์ วีรยศพงศ์ ชื่อเล่น เติ้ล";
+        $birthday = "เกิดวันที่ 7 พฤศจิกายน 2546 อายุ 21 ปี";
+        $university = "มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ 
+        </br>คณะเทคโนโลยีและการจัดการอุตสาหกรรม 
+        </br>ภาควิชาเทคโนโลยีสารสนเทศ";
+        $historystd = "ชั้นอนุบาลถึงประถมศึกษาที่    โรงเรียนสิริศึกษา
+        </br>ชั้นมัธยมศึกษาที่            โรงเรียนสมุทรปราการ";
+        $contract = "s6606021630061@email.kmutnb.ac.th";
+        echo " Profile  </br>";
+        echo " $namenn </br>";
+        echo "ปัจจุบันศึกษาอยู่ $university </br>";
+        echo " $birthday </br>";
+        echo "ประวัติการศึกษา $historystd</br>";
+        echo "contract Email : $contract";
+    ?>
+</body>
+</html>
