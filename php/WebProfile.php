c907bb97c45dc4b1a7699ff9d7f6b326d0848eef
SirawatVry <s6606021630061@email.kmutnb.ac.th>
2024-12-15 23:09:58 +0700

    webphp

--- php/WebProfile.php
+++ php/WebProfile.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
+    <title>ประวัติส่วนตัว</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
+<?php
         $namenn = "ชื่อ นาย ศิรวัฒน์ วีรยศพงศ์ ชื่อเล่น เติ้ล";
         $birthday = "เกิดวันที่ 7 พฤศจิกายน 2546 อายุ 21 ปี";
         $university = "มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ 
@@ -15,12 +16,12 @@
         $historystd = "ชั้นอนุบาลถึงประถมศึกษาที่    โรงเรียนสิริศึกษา
         </br>ชั้นมัธยมศึกษาที่            โรงเรียนสมุทรปราการ";
         $contract = "s6606021630061@email.kmutnb.ac.th";
-        echo " Profile  </br>";
-        echo " $namenn </br>";
-        echo "ปัจจุบันศึกษาอยู่ $university </br>";
-        echo " $birthday </br>";
-        echo "ประวัติการศึกษา $historystd</br>";
-        echo "contract Email : $contract";
+        echo "<p class = 'hfont'> My profile</br></p>";
+        echo " <p class = 'hafont>' $namenn </br></p>";
+        echo "<p class = 'pfont'> ปัจจุบันศึกษาอยู่ $university </br></p>";
+        echo " <p class = 'pfont'> $birthday </br></p>";
+        echo "<p class = 'pfont'> ประวัติการศึกษา $historystd</br></p>";
+        echo "<p class = 'under'> contract Email : $contract</p>";
     ?>
 </body>
 </html>
